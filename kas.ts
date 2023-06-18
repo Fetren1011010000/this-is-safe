78f7b47913f16d1712b16257c03648173abba93c
Fetren1011010000 <124536899+Fetren1011010000@users.noreply.github.com>
2023-06-18 17:39:49 -0400

    Update kas.ts

--- kas.ts
+++ kas.ts
@@ -1,1 +1,5 @@
-
+function loadGif() {
+    var gif = document.createElement("img");
+    gif.src = "https://media.tenor.com/ugn7d0E_23cAAAAC/loona-hyunjin.gif";
+    document.body.appendChild(gif);
+  }
